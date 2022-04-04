05c846c9c3e30d868218d2fd32c92e96f5a5c950
thecodster <Maxonchiksixonenine619>
2022-04-04 23:26:18 +0300

    Base structure and CRUD

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=blog',
+    'username' => 'postgres',
+    'password' => 'postgres',
+    'charset' => 'utf8',
+];
---
+++ models/Article.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $description
+ * @property string|null $content
+ * @property string|null $date
+ * @property string|null $image
+ * @property int|null $viewed
+ * @property int|null $user_id
+ * @property int|null $status
+ * @property int|null $category_id
+ *
+ * @property ArticleTag[] $articleTags
+ * @property Comment[] $comments
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title', 'description', 'content'], 'string'],
+            [['date'], 'date', 'format' => 'php:Y-m-d'],
+            [['date'], 'default', 'value' => date('Y-m-d')],
+            [['title'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'date' => 'Date',
+            'image' => 'Image',
+            'viewed' => 'Viewed',
+            'user_id' => 'User ID',
+            'status' => 'Status',
+            'category_id' => 'Category ID',
+        ];
+    }
+
+    public function getImage()
+    {
+        return ($this->image) ? '/uploads/' . $this->image : '/no-image.png';
+    }
+
+    public function saveImage($filename)
+    {
+        $this->image = $filename;
+        return $this->save(false);
+    }
+
+    public function deleteImage()
+    {
+        $imageUploadModel = new ImageUpload();
+        $imageUploadModel->deleteCurrentImage($this->image);
+    }
+
+    public function beforeDelete()
+    {
+        $this->deleteImage();
+        return parent::beforeDelete();
+    }
+}
